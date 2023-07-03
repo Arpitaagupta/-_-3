5fb935234869d8c7526a782cd027d8e6ddc24035
Arpita Gupta <arpitagupta6620@gmail.com>
2023-07-03 15:24:44 +0530

    Renamed title of app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:oibsip_task3/main.dart';
+// import 'package:oibsip_task3/main.dart';
+import 'package:Calculator/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
